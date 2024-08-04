c26c6baff0a98cb09d4f58eba0e377ef8926cba4
Olena <155203553+elgatoafk@users.noreply.github.com>
2024-08-04 21:45:10 +0300

    Update logger.py
    - Includes exception message
    - includes prints ( if any)

--- src/dependencies/logger.py
+++ src/dependencies/logger.py
@@ -1,15 +1,42 @@
-import logging
 from functools import wraps
-
-from src.config.logger_config import logger
-
+from contextlib import redirect_stdout
+import io
+from config.logger_config import logger
 
 def log_function(func):
+    """
+    Decorator that logs the call and return value of the decorated function.
+    Also logs any exceptions raised by the function and captures print statements.
+
+    Args:
+        func (Callable): The function to be decorated.
+
+    Returns:
+        Callable: The wrapped function with logging.
+    """
     @wraps(func)
     def wrapper(*args, **kwargs):
-        logger.info(f"Function '{func.__name__}' called with args: {args} and kwargs: {kwargs}")
-        result = func(*args, **kwargs)
-        logger.info(f"Function '{func.__name__}' returned {result}")
-        return result
+
+        stream = io.StringIO()
+        try:
+            logger.info(f"Function '{func.__name__}' called with args: {args} and kwargs: {kwargs}")
+            with redirect_stdout(stream):
+                result = func(*args, **kwargs)
+
+            output = stream.getvalue()
+            if output:
+                logger.info(f"Print output from '{func.__name__}':\n{output}")
+            if result is None:
+                logger.info(f"Function '{func.__name__}' completed without returning a value")
+            else:
+                logger.info(f"Function '{func.__name__}' returned {result}")
+            return result
+        except Exception as e:
+
+            output = stream.getvalue()
+            if output:
+                logger.info(f"Print output from '{func.__name__}':\n{output}")
+            logger.exception(f"Function '{func.__name__}' raised an exception: {e}")
+            raise
     return wrapper
 
